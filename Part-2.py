6ff0181f14a4f03e1e32e6d5301786d212b56b1d
srihareni <47131464+srihareni@users.noreply.github.com>
2019-03-03 22:24:25 +0530

    Create Part-2.py

---
+++ Part-2.py
@@ -0,0 +1,21 @@
+##Visualisation with BOKEH
+import bokeh
+from bokeh.plotting import figure, output_file, show
+from bokeh.io import show, output_notebook
+from bokeh.palettes import Blues9
+from bokeh.palettes import RdBu3
+from bokeh.models import ColumnDataSource, CategoricalColorMapper, ContinuousColorMapper
+from bokeh.palettes import Spectral11
+
+output_notebook()
+
+def bokehplot(stock):
+    dataset = dict(stock=stock['Close'], Date=stock.index)
+    
+    p = figure(plot_width=800, plot_height=250,  title = 'time series for {}' .format(stock.name), x_axis_type="datetime")
+    p.line(stock.index, stock['Close'], color='blue', alpha=0.5)
+    
+    #show price shock w/o vol shock
+    
+    p.circle(stock.index, stock.Close*stock["priceshock_w/0_volshock"], size=4, legend='price shock without vol shock')
+    show(p)
