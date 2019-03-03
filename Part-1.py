a94bda0f71c2ca74fc8faf3d760b2962447a0570
srihareni <47131464+srihareni@users.noreply.github.com>
2019-03-03 19:20:58 +0530

    Create Part-1.py

---
+++ Part-1.py
@@ -0,0 +1,70 @@
+#packages
+import pandas as pd
+from pandas import datetime
+import numpy as np
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+%matplotlib inline
+plt.style.use('fivethirtyeight')
+
+import seaborn as sns
+
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize'] = 15,9    #figure size
+
+#importing the data
+path = 'E:/GitHub_Project_upload/done/python-test/'
+
+TCS = pd.read_csv(path + 'TCS.csv', parse_dates=['Date'])
+
+INFY = pd.read_csv(path + 'INFY.csv', parse_dates=['Date'])
+
+NIFTY = pd.read_csv(path + 'nifty_it.csv', parse_dates=['Date'])
+
+
+stocks = [TCS, INFY, NIFTY]
+
+
+TCS.name = 'TCS'
+INFY.name = 'INFY'
+NIFTY.name = 'NIFTY_IT'
+TCS["Date"] = pd.to_datetime(TCS["Date"])
+INFY["Date"] = pd.to_datetime(INFY["Date"])
+NIFTY["Date"] = pd.to_datetime(NIFTY["Date"])
+
+#extracting the data
+def build_features(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Year'] = df['Date'].dt.year
+    df['Month'] = df.Date.dt.month
+    df['Day'] = df.Date.dt.day
+    df['WeekOfYear'] = df.Date.dt.weekofyear
+    
+for i in range(len(stocks)):
+    # print(stocks[i])
+    build_features(stocks[i])
+
+TCS.shape #for checking new features
+
+#PART - 1
+#Create 4,16,....,52 week moving average(closing price) for each stock and index. This should happen through a function.
+
+def movingaverage(values, m):
+   """caluculate average of m observations
+      m-rolling window 
+   """
+return np.average(values[-m:])
+
+def indexing(stock):
+    stock.index = stock['Date']
+    return stock
+    
+indexing(TCS)
+indexing(INFY)
+indexing(NIFTY)
+    
+    
+    
+    
